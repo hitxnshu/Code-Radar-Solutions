6be387bb5fe4eb2e6d01c189bdcabbdbf3793b87
Hitanshu Goyal <hitanshugoyal1724@gmail.com>
2025-02-23 17:47:44 +0530

    Solution to Count Students Scoring Above a Threshold

---
+++ Count Students Scoring Above a Threshold@@67877d61dc107486ad5c2747/code.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+struct record{
+    int num;
+    char name[20];
+    float marks;
+};
+int main(){
+    int n;
+    scanf("%d",&n);
+    struct record s[n];
+    for(int i = 0;i < n;i++){
+        scanf("%d",&s[i].num);
+        scanf("%s",&s[i].name);
+        scanf("%f",&s[i].marks);
+    }
+    float thr;
+    int count = 0;
+    scanf("%f",&thr);
+    for(int i = 0;i < n;i++){
+        if(s[i].marks>thr){
+            count++;
+        }
+    }
+    printf("%d",count);
+}
