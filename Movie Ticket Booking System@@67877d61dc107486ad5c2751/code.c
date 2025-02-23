ace88be45e05832caa31ef3a9e50c6b513fa1e18
Hitanshu Goyal <hitanshugoyal1724@gmail.com>
2025-02-23 12:56:51 +0530

    Solution to Movie Ticket Booking System

---
+++ Movie Ticket Booking System@@67877d61dc107486ad5c2751/code.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+struct movieticket{
+    char name[20];
+    char type[20];
+    float price;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    struct movieticket[n];
+    for(int i = 0;i < n;i++){
+        scanf("%s",&movieticket.name[i]);
+        scanf("%s",&movieticket.type[i]);
+        scanf("%f",&movieticket.price[i]);
+    }
+    for(int i = 0;i < n;i++){
+        printf("%s: %.2f, ",movieticket.type,movieticket.price);
+    }
+
+}
