752d4c78421724c99876c8642517377f84a479c6
Hitanshu Goyal <hitanshugoyal1724@gmail.com>
2025-02-23 12:46:21 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -4,13 +4,14 @@ struct library{
     char author[20];
     int price;
 };
+int main(){
 int n;
 scanf("%d",&n);
 struct library b[n];
 for(int i = 0;i < n;i++){
     scanf("%s",&b[i].title);
     scanf("%s",&b[i].author);
-    scanf("%.2f",&b[i].price);
+    scanf("%f",&b[i].price);
 }
 float threshold;
 scanf("%f",&threshold);
@@ -18,4 +19,4 @@ for(int i = 0;i < n;i++){
     if(b[i].price>=threshold){
         printf("Title: %s, Author: %s, Price: %.2f",b[i].title,b[i].author,b[i].price);
     }
-}
+}}
