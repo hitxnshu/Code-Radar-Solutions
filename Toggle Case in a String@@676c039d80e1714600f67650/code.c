d2ab6727f2db556e7ab329e0fbd6ba6e544f8216
Hitanshu Goyal <hitanshugoyal1724@gmail.com>
2025-02-25 20:46:07 +0530

    Solution to Toggle Case in a String

--- Toggle Case in a String@@676c039d80e1714600f67650/code.c
+++ Toggle Case in a String@@676c039d80e1714600f67650/code.c
@@ -5,7 +5,7 @@ int main(){
     char sen[50];
     fgets(sen,sizeof(sen),stdin);
     int length = strlen(sen);
-    for(int i = 0;i < n;i++){
+    for(int i = 0;i < length;i++){
         if(sen[i]>'a' && sen[i]<'z'){
             sen[i] = toupper(sen[i]);
             printf("%c",sen[i]);
