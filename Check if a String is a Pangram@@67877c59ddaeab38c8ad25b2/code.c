fb97116018b7a254fddc04a9b64679148ef191b6
Hitanshu Goyal <hitanshugoyal1724@gmail.com>
2025-03-20 00:28:09 +0530

    Solution to Check if a String is a Pangram

--- Check if a String is a Pangram@@67877c59ddaeab38c8ad25b2/code.c
+++ Check if a String is a Pangram@@67877c59ddaeab38c8ad25b2/code.c
@@ -3,22 +3,21 @@
 #include<ctype.h>
 int main(){
     char sen[100];
-    int pangram = 1;
+    int pangram = 0;
     fgets(sen,sizeof(sen),stdin);
     int length = strlen(sen);
-    for(char ch = 'a';ch <= 'z';ch++){
-        char ch1 = ch;
     for(int i = 0;i < length;i++){
         sen[i] = tolower(sen[i]);
-        if(sen[i]!=ch1){
-            pangram = 0;
-            printf("No");
-            break;
+        if(sen[i] == 'a' || sen[i] == 'b' || sen[i] == 'c' || sen[i] == 'd' || sen[i] == 'e' || sen[i] == 'f' || sen[i] == 'g' || sen[i] == 'h' || sen[i] == 'i' || sen[i] == 'j' || sen[i] == 'k' || sen[i] == 'l' || sen[i] == 'm' || sen[i] == 'n' || sen[i] == 'o' || sen[i] == 'p' || sen[i] == 'q' || sen[i] == 'r' || sen[i] == 's' || sen[i] == 't' || sen[i] == 'u' || sen[i] == 'v' || sen[i] == 'w' || sen[i] == 'x' || sen[i] == 'y' || sen[i] == 'z' ){
+          pangram = 1;  
         }
-    }
     }
     if(pangram){
         printf("Yes");
     }
+    else{
+        printf("No");
+    }
+
 
     }
