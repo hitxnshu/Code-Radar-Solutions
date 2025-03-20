0ee6ce1da40f654f563b483011e524e00d3287fc
Hitanshu Goyal <hitanshugoyal1724@gmail.com>
2025-03-20 20:43:36 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -11,15 +11,3 @@ int caesarCipher(char message[], int shift, char encrypted[]){
     }
 
 }
-int main(){
-    char message[100];
-    int shift;
-    char encrypted[100];
-    scanf("%[^\n]s", message);
-    scanf("%d",&shift);
-    shift = shift % 26;
-    if(shift < 0) shift += 26;
-    caesarCipher(message, shift, encrypted);
-    printf("%s\n", encrypted);
-    return 0;
-}
