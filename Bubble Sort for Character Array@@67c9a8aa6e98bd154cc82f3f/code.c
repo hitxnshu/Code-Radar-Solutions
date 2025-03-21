6121ca596e7cbd03b1d04a3ccf98f58e5e3e97eb
Hitanshu Goyal <hitanshugoyal1724@gmail.com>
2025-03-21 14:21:27 +0530

    Solution to Bubble Sort for Character Array

--- Bubble Sort for Character Array@@67c9a8aa6e98bd154cc82f3f/code.c
+++ Bubble Sort for Character Array@@67c9a8aa6e98bd154cc82f3f/code.c
@@ -1,7 +1,7 @@
 void bubbleSort(char arr[],int n){
     char ch;
     for(int i = 0;i < n-1;i++){
-        for(int j = i+1;j < n;j++){
+        for(int j = 0;j < n-i-1;j++){
             if(arr[j]>arr[j+1]){
                ch = arr[j];
                arr[j] = arr[j+1];
@@ -10,9 +10,9 @@ void bubbleSort(char arr[],int n){
         }
     }
 }
-// void printArray(char arr[], int n){
-//     printf("Sorted array: ");
-//     for(int i = 0;i < n;i++){
-//         printf("%c ",arr[i]);
-//     }
-// }
+void printArray(char arr[], int n){
+    printf("Sorted array: ");
+    for(int i = 0;i < n;i++){
+        printf("%c ",arr[i]);
+    }
+}
