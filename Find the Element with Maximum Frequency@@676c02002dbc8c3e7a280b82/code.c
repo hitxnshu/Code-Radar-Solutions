642866dec3d26282bb089eb4b6cc94d1654bd2dc
Hitanshu Goyal <hitanshugoyal1724@gmail.com>
2025-04-10 22:52:31 +0530

    Solution to Find the Element with Maximum Frequency

--- Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b82/code.c
+++ Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b82/code.c
@@ -2,37 +2,29 @@
 int main(){
     int n;
     scanf("%d",&n);
-    int arr1[n];
-    int arr2[10];
-    int temp = 0;
-    int tempo = 0;
+    int arr[n];
+    int maxCount = 0;
+    int ans;
     for(int i = 0;i < n;i++){
-        scanf("%d",&arr1[i]);
+        scanf("%d",&arr[i]);
     }
     for(int i = 0;i < n;i++){
-       temp = arr1[i];
-       int count = 0;
-       for(int j = i;j < n;j++){
-        if(arr1[j]==temp){
-            count++;
+        int count = 1;
+        for(int j = i+1;j < n;j++){
+            if(arr[i] == arr[j]){
+                count++;
+            }
         }
-       }
-       for(int i = 0;i < 10;i++){
-        arr2[i] = count;
-       }
-
-    }
-    for(int i = 0;i < n;i++){
-        tempo = arr2[i];
-        for(int j = i;j < n;j++){
-            if(arr2[j]>tempo){
-                tempo = arr2[j];
+        if(count>maxCount){
+            maxCount = count;
+            ans = arr[i];
+        }
+        else if(count == maxCount){
+            if(ans>arr[i]){
+                ans = arr[i];
             }
         }
 
     }
-    printf("%d",tempo);
-
-
-
+    printf("%d",ans);
 }
