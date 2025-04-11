801a96a728368471c232baa88ff75ae95d5e5fca
Hitanshu Goyal <hitanshugoyal1724@gmail.com>
2025-04-11 18:36:16 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -8,15 +8,25 @@ int main(){
     int n;
     scanf("%d",&n);
     struct Student s1;
+    int temp = 0;
     for(int i = 0;i < n;i++){
-    scanf("%d",&si.roll);
-    scanf("%s",&si.name);
-    scanf("%f",&si.marks);
+    scanf("%d",&s[i].roll);
+    scanf("%s",&s[i].name);
+    scanf("%f",&s[i].marks);
     }
     for(int i = 0;i < n;i++){
         for(int j = i+1;j < n;j++){
-            if(si)
+            if(s[i].marks>s[j].marks){
+                temp = s[i].marks;
+                s[i].marks = s[j].marks;
+                s[j].marks = temp;
+            }
         }
+    }
+    for(int i = 0;i < n;i++){
+        printf("%d",s[i].roll);
+        printf("%s",s[i].name);
+        printf("%f",s[i].marks);
     }
 
 }
